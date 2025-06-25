10de14fa0ab065ab7f1f7535963c33bff7b67b6e
MBilalShaikh2003 <141023004+MBilalShaikh2003@users.noreply.github.com>
2025-06-25 18:05:50 +0500

    update

--- dynamicrouting/src/app/Recipes/page.jsx
+++ dynamicrouting/src/app/Recipes/page.jsx
@@ -1,26 +1,15 @@
 import RecipeCard from "../components/RecipeCard";
 import recipes from "../data/recipe";
 
-
-export default function RecipePages(){
-    return(
-        <div>
-            <h1>All Recipes here </h1>
-            <div>
-                {
-                    recipes.map(recipe)=>(
-
-                        <RecipeCard key={recipe.id} recipe={recipe} />
-
-
-                    )
-            }
-            </div>
-
-
-        </div>
-    )
-
-
+export default function RecipePages() {
+  return (
+    <div>
+      <h1>All Recipes here</h1>
+      <div>
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
 }
-
