51ed37c64edd807cc2766568434b77d618502f3e
MBilalShaikh2003 <141023004+MBilalShaikh2003@users.noreply.github.com>
2025-06-25 16:56:12 +0500

    added header component

---
+++ dynamicrouting/src/app/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Headers = () => {
+  return (
+    <header className=' bg-red-500 p-4'>
+        <h1 className=' text-2xl font-bold'>My Recipe Book </h1>
+    </header>
+  )
+}
+
+export default Headers;
